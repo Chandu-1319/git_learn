bd26daf2c020cc148601d2df7041cefa046a3e15
A ChandraSekhar <chandrasekhar.a@makeskilled.com>
2025-06-18 16:33:42 +0530

    Replaced given with input

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 s=int(input("Enter a num: "))
 if(s%2==0):
-    print("The given num is Even")
+    print("The given input is Even")
 else:
-    print("The given num is Odd")
+    print("The given input is Odd")
